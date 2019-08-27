ddfb7f3ce4e0c5e64ab9a2bd4218321508cf0e2f
SoSIlent <1325360514@qq.com>
2019-08-27 20:07:27 +0800

    fix Console Bug add RequestHeader

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-import time,sys,requests,threading
+import time,sys,threading
+from pprint import pprint
 #PyQt GUI
-
 from PyQt5.QtWidgets import QApplication, QMainWindow
+import PyQt5.sip
 from Form import *
 #获取ID的API
 from GetID import *
@@ -22,30 +23,27 @@ class MyWindow(QMainWindow, Ui_Form):
             return 0
         SearchName = self.textEdit.toPlainText()
         time.sleep(0.1)
-        self.Console.append('[Info]正在搜索 -关键字: ' + SearchName)
         id, name = self.getSong(SearchName)
         # 如果搜索失败
         if id == None:
-            self.Console.append('[Error]未搜索到相应歌曲，尝试更换关键词')
+            self.outPutMessageToConsole('[Error]未搜索到相应歌曲，尝试更换关键词')
             return 0
         #搜索成功
-        print("获得信息：\n  下载ID:",id,"\n   歌曲名:",name)
+        print("获得信息：\n   下载ID:",id,"\n   歌曲名:",name)
         time.sleep(0.1)
-        self.Console.append('[Info]搜索到歌曲:'+ name)
+        self.outPutMessageToConsole('[Info]搜索到歌曲:'+ name)
         #如果还未设置路径（第一次执行下载）
         if self.directory == '':
-            self.isNoPath = False
-            self.directory = QtWidgets.QFileDialog.getExistingDirectory(self, "getExistingDirectory", "./")
-            self.Console.append('[Info]选定目录:' + self.directory)
+            self.directory = self.ChangeDownloadDir_Button_click()
         time.sleep(0.1)
-        self.Console.append('[Info]开始下载:' + name)
+        self.outPutMessageToConsole('[Info]开始下载:' + name)
         t = threading.Thread(target=self.download,args=(id,name,self.directory))
         t.setDaemon(False)
         t.start()
 
     def ChangeDownloadDir_Button_click(self):
         self.directory = QtWidgets.QFileDialog.getExistingDirectory(self, "getExistingDirectory", "./")
-        self.Console.append('[Info]选定目录:'+self.directory)
+        self.outPutMessageToConsole('[Info]选定目录:'+self.directory)
         print(self.directory)
         return self.directory
 
@@ -54,18 +52,22 @@ class MyWindow(QMainWindow, Ui_Form):
 
         try:
             music_url = 'http://music.163.com/song/media/outer/url?id=%s.mp3' % music_id
+            headers = {
+                'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/55.0.2883.87 Safari/537.36'
+                }#伪装 防反爬虫
             print('开始下载 ' + music_name + ' 到 ' + directory + ' 目录')
-            r = requests.get(music_url)
+            r = requests.get(url=music_url,headers=headers)
+            pprint(r)#答应Response [200]即成功
             path = directory + '\%s.mp3' % music_name  # 下载文件存放路径
             with open(path, 'wb') as f:
                 f.write(r.content)
             time.sleep(1)
             print("成功下载" + music_name)
-            self.Console.append('[Info]下载完成:' + music_name)
+            self.outPutMessageToConsole('[Info]下载完成:' + music_name)
             return 0
         except Exception as e:
-            print("错误:" + e)  # 要会员才能“播放”的歌是不能下载的
-            self.Console.append('[Error]' + e)
+            print(e)  # 要会员才能“播放”的歌是不能下载的
+            self.outPutMessageToConsole("[Error]请使用控制台查看错误")
             return 1
 
 
@@ -73,9 +75,14 @@ class MyWindow(QMainWindow, Ui_Form):
     def getSong(self,name):
         searcher = search()
         #print('[Info]Got ID:'+id +'\n[Info]Got Name:'+ name)
+        self.outPutMessageToConsole('[Info]正在搜索关键字: ' + name)
         id,name = searcher.search_id(name)
 
         return id,name
+
+    def outPutMessageToConsole(self,str):
+        self.Console.append(str)
+        self.Console.moveCursor(self.Console.textCursor().End)
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
